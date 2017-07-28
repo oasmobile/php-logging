3108b4e106af18a516ba39e5d532d3bfda9c76f7
Minhao Zhang <minhao.zhang@qq.com>
2017-07-28 18:16:02 +0800

    * auto retry when local-file handler fails first write (racing condition in multi-process env)

--- src/LocalFileHandler.php
+++ src/LocalFileHandler.php
@@ -15,26 +15,26 @@ use Monolog\Logger;
 class LocalFileHandler extends StreamHandler
 {
     use MLoggingHandlerTrait;
-
+    
     public function __construct($path = null, $namePattern = "%date%/%script%.log", $level = Logger::DEBUG)
     {
         if (!$path) {
             $path = sys_get_temp_dir();
         }
-
+        
         $translation_table = [
             "%date%"   => date('Ymd'),
             "%hour%"   => date('H'),
             "%script%" => basename($_SERVER['SCRIPT_FILENAME'], ".php"),
             "%pid%"    => getmypid(),
         ];
-
+        
         $log_file = strtr($namePattern, $translation_table);
-
+        
         $path = $path . "/" . $log_file;
-
+        
         parent::__construct($path, $level);
-
+        
         $datetime_format = "Ymd-His P";
         $output_format   = "[%channel%] %datetime% | %level_name% | %message%  %context% %extra%\n"; // %context% %extra%
         $line_formatter  = new LineFormatter(
@@ -44,7 +44,17 @@ class LocalFileHandler extends StreamHandler
             true
         );
         $line_formatter->includeStacktraces();
-
+        
         $this->setFormatter($line_formatter);
+    }
+    
+    protected function write(array $record)
+    {
+        try {
+            parent::write($record);
+        } catch (\UnexpectedValueException $e) {
+            // try again because there might be another process writing to same file/dir
+            parent::write($record); // if the second call still throws, exception will bubble
+        }
     }
 }
